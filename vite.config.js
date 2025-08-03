ca5c97432910ce8167bc3814246db5cf7d0e9e19
Jennifer Salhoff <bigbrainfoods@gmail.com>
2025-08-03 00:12:20 -0400

    Fix: Clean vite.config.js and update for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,11 @@
-export default { base: '/Big-Brain-Foods-Website/' };
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+})
+
